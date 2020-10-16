486eac7eef2731f915ab4c6c4237c8f0efd635ca
BloodyBogan <michal.kastan99@gmail.com>
2020-10-16 18:38:24 +0200

    Edited proxy and server base URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import { ApolloProvider } from '@apollo/client';
 import { setContext } from 'apollo-link-context';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:3000',
+  uri: 'https://quiet-garden-91027.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
